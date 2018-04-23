f2264ef43d42781721f7f7611c047475804bac5e
joneikholm <joneikholm@gmail.com>
2018-04-23 09:04:13 +0200

    prepared classes for teaching

--- src/binarytree/ITreeNode.java
+++ src/binarytree/ITreeNode.java
@@ -3,17 +3,4 @@ package binarytree;
 public class ITreeNode
 {
 
-	public int data;
-	public ITreeNode left;
-	public ITreeNode right;
-	
-	public ITreeNode(int data, ITreeNode left, ITreeNode right){
-		this.data=data;
-		this.left=left;
-		this.right=right;	
-	}
-	public ITreeNode(int data){
-		this.data=data;
-	}
-	
 }
