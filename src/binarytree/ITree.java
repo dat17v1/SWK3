c9407a5bc94320b550d0710caa0f172d37ec62b2
joneikholm <joneikholm@gmail.com>
2018-04-23 10:08:47 +0200

    add implemented

--- src/binarytree/ITree.java
+++ src/binarytree/ITree.java
@@ -1,6 +1,8 @@
 package binarytree;
 
 
+import java.util.*;
+
 // recursive delete: https://www.geeksforgeeks.org/binary-search-tree-set-2-delete/
 public class ITree
 {
@@ -8,16 +10,28 @@ public class ITree
 	private ITreeNode root;
 	
 	public ITree(ITreeNode node){
-
+		root = node;
 	}
 
+	public ITree(){
+
+	}
 	
 	public void add(int value){
-
+			root = add(root,value);
 	}
 
-	private ITreeNode add(ITreeNode node, int value){
-		return null;
+
+	private ITreeNode add(ITreeNode node, int value){ // value=55
+		// 1. case: Tomt træ
+		if(node == null){
+			node = new ITreeNode(value);
+		}else if(value < node.data){ // 2. rekursiv case
+			node.left = add(node.left,value);
+		}else if(value > node.data){
+			node.right = add(node.right, value);
+		}
+		return node;
 	}
 
 //	protected boolean delete(int value){
@@ -79,57 +93,57 @@ public class ITree
 
 
 
-//	public void printLevelWise() {
-//		List<List<ITreeNode>> levels = traverseLevels(root);
-//
-//		for (List<ITreeNode> level : levels) {
-//			for (ITreeNode node : level) {
-//				System.out.print(node.data + " ");
-//			}
-//			System.out.println();
-//		}
-//	}
-//
-//	private List<List<ITreeNode>> traverseLevels(ITreeNode root) {
-//		if (root == null) {
-//			return Collections.emptyList();
-//		}
-//		List<List<ITreeNode>> levels = new LinkedList<>();
-//
-//		Queue<ITreeNode> nodes = new LinkedList<>();
-//		nodes.add(root);
-//
-//		while (!nodes.isEmpty()) {
-//			List<ITreeNode> level = new ArrayList<>(nodes.size());
-//			levels.add(level);
-//
-//			for (ITreeNode node : new ArrayList<>(nodes)) {
-//				level.add(node);
-//				if (node.left != null) {
-//					nodes.add(node.left);
-//				}
-//				if (node.right != null) {
-//					nodes.add(node.right);
-//				}
-//				nodes.poll();
-//			}
-//		}
-//		return levels;
-//	}
+	public void printLevelWise() {
+		List<List<ITreeNode>> levels = traverseLevels(root);
+
+		for (List<ITreeNode> level : levels) {
+			for (ITreeNode node : level) {
+				System.out.print(node.data + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	private List<List<ITreeNode>> traverseLevels(ITreeNode root) {
+		if (root == null) {
+			return Collections.emptyList();
+		}
+		List<List<ITreeNode>> levels = new LinkedList<>();
+
+		Queue<ITreeNode> nodes = new LinkedList<>();
+		nodes.add(root);
+
+		while (!nodes.isEmpty()) {
+			List<ITreeNode> level = new ArrayList<>(nodes.size());
+			levels.add(level);
+
+			for (ITreeNode node : new ArrayList<>(nodes)) {
+				level.add(node);
+				if (node.left != null) {
+					nodes.add(node.left);
+				}
+				if (node.right != null) {
+					nodes.add(node.right);
+				}
+				nodes.poll();
+			}
+		}
+		return levels;
+	}
 	
 	public static void main(String[] args){
-//		ITreeNode n1=new ITreeNode(55);
-//		ITreeNode n2=new ITreeNode(100);
-//		ITreeNode n3=new ITreeNode(60, n1, n2);
-//
-//		ITree theTree=new ITree(n3);
-//		theTree.add(45);
-//		theTree.add(57);
-//		theTree.add(59);
-//		theTree.add(67);
-//		theTree.add(107);
-//		theTree.add(101);
-//		theTree.printLevelWise();
+		ITreeNode n1=new ITreeNode(55);
+		ITreeNode n2=new ITreeNode(100);
+		ITreeNode n3=new ITreeNode(n1, n2, 60);
+
+		ITree theTree=new ITree(n3);
+		theTree.add(45);
+		theTree.add(57);
+		theTree.add(59);
+		theTree.add(67);
+		theTree.add(107);
+		theTree.add(101);
+		theTree.printLevelWise();
 
 	}
 }
--- src/binarytree/ITreeNode.java
+++ src/binarytree/ITreeNode.java
@@ -2,5 +2,18 @@ package binarytree;
 
 public class ITreeNode
 {
+    protected ITreeNode right;
+    protected ITreeNode left;
+    protected int data;
+
+    public ITreeNode(ITreeNode right, ITreeNode left, int data) {
+        this.right = right;
+        this.left = left;
+        this.data = data;
+    }
+
+    public ITreeNode(int data) {
+        this.data = data;
+    }
 
 }
